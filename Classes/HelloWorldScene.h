efee772875d866d2fa89775abd214014c693fab6
Drake Zarowny <dzarowny@gmail.com>
2015-07-23 14:07:56 -0600

    Add member vector. This one line guarantees a crash in VS2013 and VS2015

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -6,6 +6,8 @@
 class HelloWorld : public cocos2d::Scene
 {
 public:
+	std::vector<int> myVectorMember;
+
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
 
